8caadc45e1d7a68be95dfc39af4199df28a3d8cc
william <williamlibero@profkenny.com.br>
2024-04-16 22:21:53 -0300

    Update in the projects component to display a default image if the project does not have an image.

--- src/app/projects/route.js
+++ src/app/projects/route.js
@@ -1,5 +1,7 @@
 export async function GET() {
-  const res = await fetch("https://my-projects-api.vercel.app/api/projects");
+  const res = await fetch("https://my-projects-api.vercel.app/api/projects", {
+    next: { revalidate: 60 },
+  });
   const data = await res.json();
 
   return Response.json({ data });
