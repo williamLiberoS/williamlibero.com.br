59f7a924cf4b04899ac982908b95584ad040b9fd
william <williamlibero@profkenny.com.br>
2025-02-03 20:31:02 -0300

    Clarity script.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import { Inter } from "next/font/google";
 import type { Metadata } from "next";
+import Script from "next/script";
 import "./globals.css";
 
 const inter = Inter({ subsets: ["latin"] });
@@ -11,6 +12,19 @@ export const metadata: Metadata = {
   title: "William Libero - Full stack Developer",
   description: "William Libero - Full stack Developer",
 };
+function TrackingCode() {
+  return (
+    <>
+      <Script type="text/javascript" strategy="beforeInteractive">
+        {`(function(c,l,a,r,i,t,y){
+          c[a] = c[a] || function () { (c[a].q = c[a].q || []).push(arguments) };
+        t=l.createElement(r);t.async=1;t.src="https://www.clarity.ms/tag/"+i;
+        y=l.getElementsByTagName(r)[0];y.parentNode.insertBefore(t,y);
+    })(window, document, "clarity", "script", "q4a4vt8g0p");`}
+      </Script>
+    </>
+  );
+}
 
 export default function RootLayout({
   children,
@@ -23,6 +37,7 @@ export default function RootLayout({
       className={"scroll-smooth light"}
       style={{ marginTop: "0px", colorScheme: "light" }}
     >
+      {TrackingCode()}
       <body
         className={
           inter.className + " light light:bg-gray-100 dark dark:bg-slate-900"
